fea39463ab9bb506d18cbe51e2f47d981677f240
skerdinelaj <nelajkeno@gmail.com>
2024-04-26 18:10:19 +0200

    updated

--- components/textBox.jsx
+++ components/textBox.jsx
@@ -3,11 +3,11 @@ export default function TextBox(props) {
     return (
       <>
       <form>
-        <div class="mb-1 flex justify-center">
+        <div className="mb-1 flex justify-center">
           <input
             type="text"
             id="text"
-            class="w-64 shadow-md text-center block p-2.5 text-sm rounded-lg
+            className="w-64 shadow-md text-center block p-2.5 text-sm rounded-lg
             bg-white border border-gray-300 text-gray-900 
             focus:ring-fuchsia-900 focus:border-fuchsia-900"
             placeholder={props.text}
